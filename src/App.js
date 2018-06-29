6094ab7b06e557fa1c82d73b374fbacd5c76f618
Rafael Stephano <rafa.stephano@gmail.com>
2018-06-28 23:59:32 -0300

    Alteração no layout, inclusão de imagens e etc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div id="loader"></div>
+      <div className="pure-u-1">
+        <header className="pure-u-1 pure-u-sm-2-3">
+          <div className="l-box">
+            <h1>Rafael de Oliveira Stephano</h1>
+            <p>Analista Programador Java</p>
+          </div>
+        </header>
+        <div className="pure-u-1  pure-u-sm-1-3 box">
+          <div className="l-box">
+            <i className="fas fa-home fa-2x box-icon"></i>
+            <p>Barueri - SP</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
